65f02872726f7863ef9c3f4c70c0bc0f06125444
Arturs Ozolins <arturs.ozolins94@gmail.com>
2021-11-02 19:39:16 +0200

    todoapp beginning

---
+++ resources/views/tasks/indexTask.blade.php
@@ -0,0 +1,34 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Tasks') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h2>Your current Tasks:</h2>
+                    @foreach($tasks as $task)
+                        <ul>
+                            <li>
+                                <h2>{{ $task->title }}</h2>
+                                <p>{{ $task->content }}</p>
+
+                                <a href="{{ route('tasks.edit', $task) }}">EDIT</a>
+                                <form method="post" action="{{ route('tasks.destroy', $task) }}">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit">DELETE</button>
+                                </form>
+                            </li>
+                        </ul>
+                    @endforeach
+                </div>
+                <a href="{{ route('tasks.create') }}">Create Task</a>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
---
+++ resources/views/tasks/taskCreateForm.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task application</title>
+</head>
+<body>
+    <h1>New task form:</h1>
+    <form method="post" action="{{ '/storeTask' }}">
+        <p><label for="name">Enter task title:</label></p>
+        <input id="name" type="text" name="title" placeholder="title">
+
+        <p><label for="content">Enter task description:</label></p>
+            <textarea id="content" name="content" rows="5" cols="40" placeholder="content"></textarea>
+
+        <p><button type="submit">Submit</button></p>
+    </form>
+</body>
+</html>
